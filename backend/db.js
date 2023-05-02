7b35f6e77e2679fbc130c571fd71e2d75c55d120
Udaya <udayapratapsinghgame@gmail.com>
2023-05-02 18:29:36 +0530

    backend1

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+const mongoose= require('mongoose');
+const mongoURI='mongodb://127.0.0.1:27017/';
+
+const connectToMongo= ()=>{
+    mongoose.connect(mongoURI).then( ()=>{
+        console.log("Connected to mongo");
+    })
+}
+
+module.exports= connectToMongo;
